b2c99b30374403d0a0d887f14c0ef4d5c197cac6
Ibraheem <iejibode@gmail.com>
2023-04-05 11:41:37 +0100

    final

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,19 @@ import data from './data';
 import Question from './Question';
 const App = () => {
   const [questions, setQuestions] = useState(data);
+  const [activeId, setActiveId] = useState(null);
+
+  const toggleQuestion = (id) => {
+    const newActiveId = id === activeId ? null : id;
+    setActiveId(newActiveId);
+  };
   return (
     <main>
-      <Question questions={questions} />
+      <Question
+        questions={questions}
+        activeId={activeId}
+        toggleQuestion={toggleQuestion}
+      />
     </main>
   );
 };
--- src/Question.jsx
+++ src/Question.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import SingleQuestion from './SingleQuestion';
 
-function Question({ questions }) {
+function Question({ questions, activeId, toggleQuestion }) {
   return (
     <section className="container">
       <h2>question</h2>
       {questions.map((item) => {
-        return <SingleQuestion key={item.id} {...item} />;
+        return (
+          <SingleQuestion
+            key={item.id}
+            {...item}
+            activeId={activeId}
+            toggleQuestion={toggleQuestion}
+          />
+        );
       })}
     </section>
   );
